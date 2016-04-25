e631b5314ecfeb895f2f8fc7890458a6a84d0d8d
Andraz <andraz@glit.si>
2016-04-25 09:02:26 +0200

    integrate frello

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ version 0.15.1.
 
 ## Build & development
 
-Run `grunt` for building and `grunt serve` for preview.
+Run `gulp build` for building and `gulp serve` for preview.
 
-## Testing
-
-Running `grunt test` will run the unit tests with karma.
+- gulp build
+- gulp serve:prod
+- gulp serve
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,94 @@
 
 /**
  * @ngdoc function
- * @name appFrelloApp.controller:MainCtrl
+ * @name appFrelloApp.controller:MainController
  * @description
- * # MainCtrl
+ * # MainController
  * Controller of the appFrelloApp
  */
 angular.module('appFrelloApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainController', function ($scope, TodoFactory) {
+
+      /*
+       $scope.task = {
+       desc: '',
+       isCompleted: false
+       };
+       */
+      $scope.todosList1 = TodoFactory.todos;
+      $scope.todosList2 = TodoFactory.todos;
+
+      $scope.addTask = function(){
+        TodoFactory.addTask($scope.task);
+        $scope.task = TodoFactory.resetTask();
+      };
+
+      $scope.removeTask = function(task){
+        TodoFactory.removeTask(task);
+      };
+
+      $scope.$on('fired.event', function(event, data){
+        $scope.data = 'Dodano';
+        console.log('dodatno');
+      });
+
+
+      $scope.fireEvent = function()
+      {
+        //$rootScope.$broadcast('fired.event', {data: 'Data to be sent'});
+      };
+
   });
+
+//  factory
+angular.module('appFrelloApp')
+  .factory('TodoFactory', function() {
+      var todos = [];
+
+      return {
+        todos: todos,
+        addTask: addTask,
+        removeTask: removeTask,
+        isEmpty: isEmpty,
+        isDuplicated: isDuplicated,
+        resetTask: resetTask
+      };
+
+      function isDuplicated(task){
+        var duplicate = false;
+
+        todos.forEach(function(todo) {
+          if(todo.desc === task.desc){
+            duplicate = true;
+          }
+        });
+
+        return duplicate;
+      }
+
+      function isEmpty(task){
+        var empty = false;
+        console.log(task.desc);
+        if(task.desc == '' || typeof task.desc == "undefined"){
+          empty = true;
+        }
+
+        return empty;
+      }
+
+      function addTask(task){
+        if(isEmpty(task)) return;
+        if(isDuplicated(task)) return;
+
+        todos.push(task);
+      }
+
+      function removeTask(task){
+        todos.splice(todos.indexOf(task), 1);
+      }
+
+      function resetTask(){
+        return {desc: '', isCompleted: false};
+      }
+
+});
