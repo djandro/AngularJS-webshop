8579629ad5f828dd4b7c2a2329c5d0120a12ac3a
Andraz <andraz@glit.si>
2016-04-28 16:23:24 +0200

    factory moved to singe file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the appFrelloApp
  */
 angular.module('appFrelloApp')
-  .controller('MainController', function ($scope, todoFactory) {
+  .controller('MainController', ['$scope', 'todoFactory', function ($scope, todoFactory) {
 
       $scope.todosList1 = todoFactory.todos;
       $scope.todosList2 = todoFactory.todos;
@@ -21,67 +21,6 @@ angular.module('appFrelloApp')
       $scope.removeTask = function(task){
         todoFactory.removeTask(task);
       };
-
-  });
-
-
-angular.module('appFrelloApp')
-  .factory('todoFactory', ['toastr', function(toastr) {
-    var todos = [];
-
-    return {
-      todos: todos,
-      addTask: addTask,
-      removeTask: removeTask,
-      isEmpty: isEmpty,
-      isDuplicated: isDuplicated,
-      resetTask: resetTask
-    };
-
-    function isDuplicated(task){
-      var duplicate = false;
-
-      todos.forEach(function(todo) {
-        if(todo.desc === task.desc){
-          duplicate = true;
-        }
-      });
-
-      return duplicate;
-    }
-
-    function isEmpty(task){
-      var empty = false;
-      console.log(task.desc);
-      if(task.desc == '' || typeof task.desc == "undefined"){
-        empty = true;
-      }
-
-      return empty;
-    }
-
-    function addTask(task){
-      if(isEmpty(task)) {
-        toastr.error('Vnesi ime taska!');
-        return;
-      }
-      if(isDuplicated(task)) {
-        toastr.error(task.desc, 'Ime taska že obstaja!');
-        return;
-      }
-
-      todos.push(task);
-      toastr.success(task.desc, 'Uspešno si dodal task');
-    }
-
-    function removeTask(task){
-      todos.splice(todos.indexOf(task), 1);
-      toastr.info(task.desc, 'Zbrisal si task');
-    }
-
-    function resetTask(){
-      return {desc: '', isCompleted: false};
-    }
 
   }]);
 
